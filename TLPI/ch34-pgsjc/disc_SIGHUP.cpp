259bbd4c2a638a056225487990399ea2ec8b92bd
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2023-12-27 13:13:30 +0800

    minor format

--- TLPI/ch34-pgsjc/disc_SIGHUP.cpp
+++ TLPI/ch34-pgsjc/disc_SIGHUP.cpp
@@ -31,52 +31,56 @@
 static void             /* Handler for SIGHUP */
 handler(int sig)
 {
-    printf("PID %ld: caught signal %2d (%s)\n", (long) getpid(),
-            sig, strsignal(sig));
-                        /* UNSAFE (see Section 21.1.2) */
+	printf("PID %ld: caught signal %2d (%s)\n", (long) getpid(),
+			sig, strsignal(sig));
+						/* UNSAFE (see Section 21.1.2) */
 }
 
 int
 main(int argc, char *argv[])
 {
-    pid_t parentPid, childPid;
-    int j;
-    struct sigaction sa;
+	pid_t parentPid = 0, childPid = 0;
+	int j = 0;
+	struct sigaction sa = {};
 
-    if (argc < 2 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s {d|s}... [ > sig.log 2>&1 ]\n", argv[0]);
+	if (argc < 2 || strcmp(argv[1], "--help") == 0)
+		usageErr("%s {d|s}... [ > sig.log 2>&1 ]\n", argv[0]);
 
-    setbuf(stdout, NULL);               /* Make stdout unbuffered */
+	setbuf(stdout, NULL);              /* Make stdout unbuffered */
 
-    parentPid = getpid();
-    printf("PID of parent process is:       %ld\n", (long) parentPid);
-    printf("Foreground process group ID is: %ld\n",
-            (long) tcgetpgrp(STDIN_FILENO));
+	parentPid = getpid();
+	printf("PID of parent process is:       %ld\n", (long) parentPid);
+	printf("Foreground process group ID is: %ld\n",
+			(long) tcgetpgrp(STDIN_FILENO));
 
-    for (j = 1; j < argc; j++) {        /* Create child processes */
-        childPid = fork();
-        if (childPid == -1)
-            errExit("fork");
+	for (j = 1; j < argc; j++)         /* Create child processes      (2) */
+	{
+		childPid = fork();
+		if (childPid == -1)
+			errExit("fork");
 
-        if (childPid == 0) {            /* If child... */
-            if (argv[j][0] == 'd')      /* 'd' --> to different pgrp */
-                if (setpgid(0, 0) == -1)
-                    errExit("setpgid");
+		if (childPid == 0)             /* If child...  */
+		{
+			if (argv[j][0] == 'd')     /* 'd' --> to different pgrp   (3) */
+			{
+				if (setpgid(0, 0) == -1)
+					errExit("setpgid");
+			}
 
-            sigemptyset(&sa.sa_mask);
-            sa.sa_flags = 0;
-            sa.sa_handler = handler;
-            if (sigaction(SIGHUP, &sa, NULL) == -1)
-                errExit("sigaction");
-            break;                      /* Child exits loop */
-        }
-    }
+			sigemptyset(&sa.sa_mask);
+			sa.sa_flags = 0;
+			sa.sa_handler = handler;
+			if (sigaction(SIGHUP, &sa, NULL) == -1) //                (4)
+				errExit("sigaction");
+			break;                      /* Child exits loop */
+		}
+	}
 
-    /* All processes fall through to here */
+	/* All processes fall through to here */
 
-    alarm(60);      /* Ensure each process eventually terminates */
+	alarm(60);      /* Ensure each process eventually terminates      (5) */
 
-    printf("PID=%ld PGID=%ld\n", (long) getpid(), (long) getpgrp());
-    for (;;)
-        pause();        /* Wait for signals */
+	printf("PID=%ld PGID=%ld\n", (long)getpid(), (long)getpgrp()); // (6)
+	for (;;)
+		pause();        /* Wait for signals                           (7) */
 }
