e61abadb4a7dd58ced1429e92790c2c42df1d380
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2023-12-28 20:50:50 +0800

    minor formatting

--- TLPI/ch34-pgsjc/job_mon.cpp
+++ TLPI/ch34-pgsjc/job_mon.cpp
@@ -49,19 +49,19 @@ handler(int sig)
 	/* UNSAFE: This handler uses non-async-signal-safe functions
 	   (fprintf(), strsignal(); see Section 21.1.2) */
 
-	if (getpid() == getpgrp())          /* If process group leader */
+	if (getpid() == getpgrp())         // If process group leader    (1)
 	{
 		fprintf(stderr, "Terminal FG process group: %ld\n",
 			(long)tcgetpgrp(STDERR_FILENO));
 	}
 
-	fprintf(stderr, "Process %ld (%d) received signal %d (%s)\n",
-				(long) getpid(), cmdNum, sig, strsignal(sig));
+	fprintf(stderr, "Process %ld (%d) received signal %d (%s)\n", // (2)
+			(long) getpid(), cmdNum, sig, strsignal(sig));
 
 	/* If we catch SIGTSTP, it won't actually stop us. Therefore we
 	   raise SIGSTOP so we actually get stopped. */
 
-	if (sig == SIGTSTP)
+	if (sig == SIGTSTP)                                           // (3)
 		raise(SIGSTOP);
 }
 
@@ -73,7 +73,7 @@ main(int argc, char *argv[])
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	sa.sa_handler = handler;
-	if (sigaction(SIGINT, &sa, NULL) == -1)
+	if (sigaction(SIGINT, &sa, NULL) == -1)                       // (4)
 		errExit("sigaction");
 	if (sigaction(SIGTSTP, &sa, NULL) == -1)
 		errExit("sigaction");
@@ -83,19 +83,26 @@ main(int argc, char *argv[])
 	/* If stdin is a terminal, this is the first process in pipeline:
 	   print a heading and initialize message to be sent down pipe */
 
-	if (isatty(STDIN_FILENO)) {
+	if (isatty(STDIN_FILENO)) {                                   // (5)
+
 		fprintf(stderr, "Terminal FG process group: %ld\n",
 				(long) tcgetpgrp(STDIN_FILENO));
-		fprintf(stderr, "Command   PID  PPID  PGRP   SID\n");
+
+		fprintf(stderr, "Command   PID  PPID  PGRP   SID\n");     // (6)
+
 		cmdNum = 0;
 
-	} else {            /* Not first in pipeline, so read message from pipe */
-		if (read(STDIN_FILENO, &cmdNum, sizeof(cmdNum)) <= 0)
-			fatal("read got EOF or error");
+	} else {
+		/* Not first in pipeline, so read message from pipe */
+
+		if (read(STDIN_FILENO, &cmdNum, sizeof(cmdNum)) <= 0)     // (7)
+			fatal("read() got EOF or error");
 	}
 
-	cmdNum++;
-	fprintf(stderr, "%4d    %5ld %5ld %5ld %5ld\n", cmdNum,
+	cmdNum++;                                                     // (8)
+	
+	fprintf(stderr, "%4d    %5ld %5ld %5ld %5ld\n",               // (9)
+			cmdNum,
 			(long) getpid(), (long) getppid(),
 			(long) getpgrp(), (long) getsid(0));
 
@@ -103,10 +110,10 @@ main(int argc, char *argv[])
 
 	if (!isatty(STDOUT_FILENO))   /* If not tty, then should be pipe */
 	{
-		if (write(STDOUT_FILENO, &cmdNum, sizeof(cmdNum)) == -1)
-			errMsg("write");
+		if (write(STDOUT_FILENO, &cmdNum, sizeof(cmdNum)) == -1)  // (10)
+			errMsg("write()");
 	}
 	
-	for (;;)            /* Wait for signals */
+	for (;;)      // Wait for signals                                (11)
 		pause();
 }
