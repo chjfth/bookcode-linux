b940152c0b86f0690b67b3d1187112499e09de9a
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-03 00:53:07 +0800

    Now the EXE vcxproj imports exe-share.props to acquire shared compiler/linker options. 00_exe_template.vcxproj updated accordingly.

--- TLPI/ch20-signals/ouch.c
+++ TLPI/ch20-signals/ouch.c
@@ -23,26 +23,26 @@
 static void
 sigHandler(int sig)
 {
-    printf("Ouch!\n");                  /* UNSAFE (see Section 21.1.2) */
+	printf("Ouch!\n");                  /* UNSAFE (see Section 21.1.2) */
 }
 
 int
 main(int argc, char *argv[])
 {
-    int j;
+	int j;
 
-    /* Establish handler for SIGINT. Here we use the simpler signal()
-       API to establish a signal handler, but for the reasons described in
-       Section 22.7 of TLPI, sigaction() is the (strongly) preferred API
-       for this task. */
+	/* Establish handler for SIGINT. Here we use the simpler signal()
+	   API to establish a signal handler, but for the reasons described in
+	   Section 22.7 of TLPI, sigaction() is the (strongly) preferred API
+	   for this task. */
 
-    if (signal(SIGINT, sigHandler) == SIG_ERR)
-        errExit("signal");
+	if (signal(SIGINT, sigHandler) == SIG_ERR)
+		errExit("signal");
 
-    /* Loop continuously waiting for signals to be delivered */
+	/* Loop continuously waiting for signals to be delivered */
 
-    for (j = 0; ; j++) {
-        printf("%d\n", j);
-        sleep(3);                       /* Loop slowly... */
-    }
+	for (j = 0; ; j++) {
+		printf("%d\n", j);
+		sleep(3);                       /* Loop slowly... */
+	}
 }
