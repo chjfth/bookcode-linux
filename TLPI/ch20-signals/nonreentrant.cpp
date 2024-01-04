7f5549c4f7428a705045e8fabe9ced50608eff7d
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-04 16:00:16 +0800

    Add CH21, nonreentrant.cpp

--- TLPI/ch20-signals/nonreentrant.cpp
+++ TLPI/ch20-signals/nonreentrant.cpp
@@ -29,42 +29,42 @@ static int handled = 0;         /* Counts number of calls to handler */
 static void
 handler(int sig)
 {
-    crypt(str2, "xx");
-    handled++;
+	crypt(str2, "xx");
+	handled++;
 }
 
 int
 main(int argc, char *argv[])
 {
-    char *cr1;
-    int callNum, mismatch;
-    struct sigaction sa;
+	char *cr1;
+	int callNum, mismatch;
+	struct sigaction sa;
 
-    if (argc != 3)
-        usageErr("%s str1 str2\n", argv[0]);
+	if (argc != 3)
+		usageErr("%s str1 str2\n", argv[0]);
 
-    str2 = argv[2];                      /* Make argv[2] available to handler */
-    cr1 = strdup(crypt(argv[1], "xx"));  /* Copy statically allocated string
-                                            to another buffer */
-    if (cr1 == NULL)
-        errExit("strdup");
+	str2 = argv[2];                      /* Make argv[2] available to handler */
+	cr1 = strdup(crypt(argv[1], "xx"));  /* Copy statically allocated string
+											to another buffer */
+	if (cr1 == NULL)
+		errExit("strdup");
 
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sa.sa_handler = handler;
-    if (sigaction(SIGINT, &sa, NULL) == -1)
-        errExit("sigaction");
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handler;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		errExit("sigaction");
 
-    /* Repeatedly call crypt() using argv[1]. If interrupted by a
-       signal handler, then the static storage returned by crypt()
-       will be overwritten by the results of encrypting argv[2], and
-       strcmp() will detect a mismatch with the value in 'cr1'. */
+	/* Repeatedly call crypt() using argv[1]. If interrupted by a
+	   signal handler, then the static storage returned by crypt()
+	   will be overwritten by the results of encrypting argv[2], and
+	   strcmp() will detect a mismatch with the value in 'cr1'. */
 
-    for (callNum = 1, mismatch = 0; ; callNum++) {
-        if (strcmp(crypt(argv[1], "xx"), cr1) != 0) {
-            mismatch++;
-            printf("Mismatch on call %d (mismatch=%d handled=%d)\n",
-                    callNum, mismatch, handled);
-        }
-    }
+	for (callNum = 1, mismatch = 0; ; callNum++) {
+		if (strcmp(crypt(argv[1], "xx"), cr1) != 0) {
+			mismatch++;
+			printf("Mismatch on call %d (mismatch=%d handled=%d)\n",
+					callNum, mismatch, handled);
+		}
+	}
 }
