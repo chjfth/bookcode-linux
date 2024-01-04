2d90ac958d27eadc53bd49492ead47d6406636be
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-04 20:34:52 +0800

    CH20, add sigmask_longjmp and sigmask_siglongjmp

--- TLPI/ch20-signals/sigmask_longjmp.cpp
+++ TLPI/ch20-signals/sigmask_longjmp.cpp
@@ -26,8 +26,8 @@
 #include "tlpi_hdr.h"
 
 static volatile sig_atomic_t canJump = 0;
-                        /* Set to 1 once "env" buffer has been
-                           initialized by [sig]setjmp() */
+						/* Set to 1 once "env" buffer has been
+						   initialized by [sig]setjmp() */
 #ifdef USE_SIGSETJMP
 static sigjmp_buf senv;
 #else
@@ -37,50 +37,50 @@ static jmp_buf env;
 static void
 handler(int sig)
 {
-    /* UNSAFE: This handler uses non-async-signal-safe functions
-       (printf(), strsignal(), printSigMask(); see Section 21.1.2) */
+	/* UNSAFE: This handler uses non-async-signal-safe functions
+	   (printf(), strsignal(), printSigMask(); see Section 21.1.2) */
 
-    printf("Received signal %d (%s), signal mask is:\n", sig,
-            strsignal(sig));
-    printSigMask(stdout, NULL);
+	printf("Received signal %d (%s), signal mask is:\n", sig,
+			strsignal(sig));
+	printSigMask(stdout, NULL);
 
-    if (!canJump) {
-        printf("'env' buffer not yet set, doing a simple return\n");
-        return;
-    }
+	if (!canJump) {
+		printf("'env' buffer not yet set, doing a simple return\n");
+		return;
+	}
 
 #ifdef USE_SIGSETJMP
-    siglongjmp(senv, 1);
+	siglongjmp(senv, 1);
 #else
-    longjmp(env, 1);
+	longjmp(env, 1);
 #endif
 }
 
 int
 main(int argc, char *argv[])
 {
-    struct sigaction sa;
+	struct sigaction sa;
 
-    printSigMask(stdout, "Signal mask at startup:\n");
+	printSigMask(stdout, "Signal mask at startup:\n");
 
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sa.sa_handler = handler;
-    if (sigaction(SIGINT, &sa, NULL) == -1)
-        errExit("sigaction");
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handler;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		errExit("sigaction");
 
 #ifdef USE_SIGSETJMP
-    printf("Calling sigsetjmp()\n");
-    if (sigsetjmp(senv, 1) == 0)
+	printf("Calling sigsetjmp()\n");
+	if (sigsetjmp(senv, 1) == 0)
 #else
-    printf("Calling setjmp()\n");
-    if (setjmp(env) == 0)
+	printf("Calling setjmp()\n");
+	if (setjmp(env) == 0)
 #endif
-        canJump = 1;                    /* Executed after [sig]setjmp() */
+		canJump = 1;                    /* Executed after [sig]setjmp() */
 
-    else                                /* Executed after [sig]longjmp() */
-        printSigMask(stdout, "After jump from handler, signal mask is:\n" );
+	else                                /* Executed after [sig]longjmp() */
+		printSigMask(stdout, "After jump from handler, signal mask is:\n" );
 
-    for (;;)                            /* Wait for signals until killed */
-        pause();
+	for (;;)                            /* Wait for signals until killed */
+		pause();
 }
