727fed8516fcfe4406dfbb81ea632340967c8377
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-04 23:08:31 +0800

    CH20, add t_sigaltstack

--- TLPI/ch20-signals/t_sigaltstack.cpp
+++ TLPI/ch20-signals/t_sigaltstack.cpp
@@ -23,16 +23,16 @@
 static void
 sigsegvHandler(int sig)
 {
-    int x;
+	int x;
 
-    /* UNSAFE: This handler uses non-async-signal-safe functions
-       (printf(), strsignal(), fflush(); see Section 21.1.2) */
+	/* UNSAFE: This handler uses non-async-signal-safe functions
+	   (printf(), strsignal(), fflush(); see Section 21.1.2) */
 
-    printf("Caught signal %d (%s)\n", sig, strsignal(sig));
-    printf("Top of handler stack near     %10p\n", (void *) &x);
-    fflush(NULL);
+	printf("Caught signal %d (%s)\n", sig, strsignal(sig));
+	printf("Top of handler stack near     %10p\n", (void *) &x);
+	fflush(NULL);
 
-    _exit(EXIT_FAILURE);                /* Can't return after SIGSEGV */
+	_exit(EXIT_FAILURE);                /* Can't return after SIGSEGV */
 }
 
 /* The following stops 'gcc -Wall' complaining that "control reaches
@@ -47,39 +47,40 @@ overflowStack(int callNum) __attribute__ ((__noreturn__));
 static void             /* A recursive function that overflows the stack */
 overflowStack(int callNum)
 {
-    char a[100000];                     /* Make this stack frame large */
+	char a[100000];                     /* Make this stack frame large */
 
-    printf("Call %4d - top of stack near %10p\n", callNum, &a[0]);
-    overflowStack(callNum+1);
+	printf("Call %4d - top of stack near %10p\n", callNum, &a[0]);
+	overflowStack(callNum+1);
 }
 
 int
 main(int argc, char *argv[])
 {
-    stack_t sigstack;
-    struct sigaction sa;
-    int j;
+	stack_t sigstack = {};
+	struct sigaction sa = {};
+	int j = 0xCDCDCDCD;
 
-    printf("Top of standard stack is near %10p\n", (void *) &j);
+	printf("Top of standard stack is near %10p\n", (void *) &j);
 
-    /* Allocate alternate stack and inform kernel of its existence */
+	/* Allocate alternate stack and inform kernel of its existence */
 
-    sigstack.ss_sp = malloc(SIGSTKSZ);
-    if (sigstack.ss_sp == NULL)
-        errExit("malloc");
+	sigstack.ss_sp = malloc(SIGSTKSZ);
+	if (sigstack.ss_sp == NULL)
+		errExit("malloc");
 
-    sigstack.ss_size = SIGSTKSZ;
-    sigstack.ss_flags = 0;
-    if (sigaltstack(&sigstack, NULL) == -1)
-        errExit("sigaltstack");
-    printf("Alternate stack is at         %10p-%p\n",
-            sigstack.ss_sp, (char *) sbrk(0) - 1);
+	sigstack.ss_size = SIGSTKSZ;
+	sigstack.ss_flags = 0;
+	if (sigaltstack(&sigstack, NULL) == -1)
+		errExit("sigaltstack");
 
-    sa.sa_handler = sigsegvHandler;     /* Establish handler for SIGSEGV */
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_ONSTACK;           /* Handler uses alternate stack */
-    if (sigaction(SIGSEGV, &sa, NULL) == -1)
-        errExit("sigaction");
+	printf("Alternate stack is at         %10p-%p\n",
+			sigstack.ss_sp, (char *)sbrk(0) - 1);
 
-    overflowStack(1);
+	sa.sa_handler = sigsegvHandler;     /* Establish handler for SIGSEGV */
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_ONSTACK;           /* Handler uses alternate stack */
+	if (sigaction(SIGSEGV, &sa, NULL) == -1)
+		errExit("sigaction");
+
+	overflowStack(1);
 }
