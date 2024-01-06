a746247987f1eda19cb01d49db994d34d054888f
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-06 17:14:57 +0800

    CH22, t_sigqueue and catch_rtsigs, realtime signal demo code.

--- TLPI/ch20-signals/t_sigqueue.cpp
+++ TLPI/ch20-signals/t_sigqueue.cpp
@@ -14,7 +14,7 @@
 
    Demonstrate the use of sigqueue() to send a (realtime) signal.
 
-   Usage: t_sigqueue sig pid data num-sigs
+   Usage: t_sigqueue sig pid data [num-sigs]
 
    Send 'num-sigs' instances of the signal 'sig' (specified as an integer), with
    accompanying data 'data' (an integer), to the process with the PID 'pid'.
@@ -26,28 +26,28 @@
 int
 main(int argc, char *argv[])
 {
-    int sig, numSigs, j, sigData;
-    union sigval sv;
+	int sig, numSigs, j, sigData;
+	union sigval sv;
 
-    if (argc < 4 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s pid sig-num data [num-sigs]\n", argv[0]);
+	if (argc < 4 || strcmp(argv[1], "--help") == 0)
+		usageErr("%s pid sig-num data [num-sigs]\n", argv[0]);
 
-    /* Display our PID and UID, so that they can be compared with the
-       corresponding fields of the siginfo_t argument supplied to the
-       handler in the receiving process */
+	/* Display our PID and UID, so that they can be compared with the
+	   corresponding fields of the siginfo_t argument supplied to the
+	   handler in the receiving process */
 
-    printf("%s: PID is %ld, UID is %ld\n", argv[0],
-            (long) getpid(), (long) getuid());
+	printf("%s: PID is %ld, UID is %ld\n", argv[0],
+			(long) getpid(), (long) getuid());
 
-    sig = getInt(argv[2], 0, "sig-num");
-    sigData = getInt(argv[3], GN_ANY_BASE, "data");
-    numSigs = (argc > 4) ? getInt(argv[4], GN_GT_0, "num-sigs") : 1;
+	sig = getInt(argv[2], 0, "sig-num");
+	sigData = getInt(argv[3], GN_ANY_BASE, "data");
+	numSigs = (argc > 4) ? getInt(argv[4], GN_GT_0, "num-sigs") : 1;
 
-    for (j = 0; j < numSigs; j++) {
-        sv.sival_int = sigData + j;
-        if (sigqueue(getLong(argv[1], 0, "pid"), sig, sv) == -1)
-            errExit("sigqueue %d", j);
-    }
+	for (j = 0; j < numSigs; j++) {
+		sv.sival_int = sigData + j;
+		if (sigqueue(getLong(argv[1], 0, "pid"), sig, sv) == -1)
+			errExit("sigqueue %d", j);
+	}
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
