97275d010247ecf71e84e36f8cdf0c1f4f5f20ce
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-07 17:07:10 +0800

    CH22, Add 22-7 signalfd_sigval.cpp

--- TLPI/ch20-signals/signalfd_sigval.cpp
+++ TLPI/ch20-signals/signalfd_sigval.cpp
@@ -19,44 +19,45 @@
    This program is Linux-specific. The signalfd API is supported since kernel
    2.6.22.
 */
-#include <sys/signalfd.h>
+#include <sys/signalfd.h> // for struct signalfd_siginfo
 #include <signal.h>
 #include "tlpi_hdr.h"
 
 int
 main(int argc, char *argv[])
 {
-    sigset_t mask;
-    int sfd, j;
-    struct signalfd_siginfo fdsi;
-    ssize_t s;
+	sigset_t mask = {};
+	int sfd = 0, j = 0;
+	struct signalfd_siginfo fdsi = {};
+	ssize_t s = 0;
 
-    if (argc < 2 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s sig-num...\n", argv[0]);
+	if (argc < 2 || strcmp(argv[1], "--help") == 0)
+		usageErr("%s sig-num...\n", argv[0]);
 
-    printf("%s: PID = %ld\n", argv[0], (long) getpid());
+	printf("%s: PID = %ld\n", argv[0], (long) getpid());
 
-    sigemptyset(&mask);
-    for (j = 1; j < argc; j++)
-        sigaddset(&mask, atoi(argv[j]));
+	sigemptyset(&mask);
+	for (j = 1; j < argc; j++)
+		sigaddset(&mask, atoi(argv[j]));
 
-    if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1)
-        errExit("sigprocmask");
+	if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1)
+		errExit("sigprocmask");
 
-    sfd = signalfd(-1, &mask, 0);
-    if (sfd == -1)
-        errExit("signalfd");
+	sfd = signalfd(-1, &mask, 0);
+	if (sfd == -1)
+		errExit("signalfd");
 
-    for (;;) {
-        s = read(sfd, &fdsi, sizeof(struct signalfd_siginfo));
-        if (s != sizeof(struct signalfd_siginfo))
-            errExit("read");
+	for (;;) {
+		s = read(sfd, &fdsi, sizeof(struct signalfd_siginfo));
+		
+		if (s != sizeof(struct signalfd_siginfo))
+			errExit("read");
 
-        printf("%s: got signal %d", argv[0], fdsi.ssi_signo);
-        if (fdsi.ssi_code == SI_QUEUE) {
-            printf("; ssi_pid = %d; ", fdsi.ssi_pid);
-            printf("ssi_int = %d", fdsi.ssi_int);
-        }
-        printf("\n");
-    }
+		printf("%s: got signal %d", argv[0], fdsi.ssi_signo);
+		if (fdsi.ssi_code == SI_QUEUE) {
+			printf("; ssi_pid = %d; ", fdsi.ssi_pid);
+			printf("ssi_int = %d", fdsi.ssi_int);
+		}
+		printf("\n");
+	}
 }
