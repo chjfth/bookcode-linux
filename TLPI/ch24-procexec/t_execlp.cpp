26085ab4da39aa803ab5fb271c8e98a041ce3e6e
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-09 06:20:05 +0800

    CH27, t_execlp.cpp

--- TLPI/ch24-procexec/t_execlp.cpp
+++ TLPI/ch24-procexec/t_execlp.cpp
@@ -19,11 +19,12 @@
 int
 main(int argc, char *argv[])
 {
-    if (argc != 2 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s pathname\n", argv[0]);
+	if (argc != 2 || strcmp(argv[1], "--help") == 0)
+		usageErr("%s pathname\n", argv[0]);
 
-    /* Execute the program specified in argv[1] */
+	/* Execute the program specified in argv[1] */
 
-    execlp(argv[1], argv[1], "hello world", (char *) NULL);
-    errExit("execlp");      /* If we get here, something went wrong */
+	execlp(argv[1], argv[1], "hello world", (char *) NULL);
+
+	errExit("execlp");      /* If we get here, something went wrong */
 }
