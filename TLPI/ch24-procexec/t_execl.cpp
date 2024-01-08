3a2c350ccab55237cdeb26b9577dbbed479f861a
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-09 06:34:21 +0800

    CH27, t_execl.cpp

--- TLPI/ch24-procexec/t_execl.cpp
+++ TLPI/ch24-procexec/t_execl.cpp
@@ -13,6 +13,8 @@
 /* t_execl.c
 
    Demonstrate the use of execl() to execute printenv(1).
+
+   New program inherits env-vars from the old program.
 */
 #include <stdlib.h>
 #include "tlpi_hdr.h"
@@ -20,12 +22,16 @@
 int
 main(int argc, char *argv[])
 {
-    printf("Initial value of USER: %s\n", getenv("USER"));
-    if (putenv("USER=britta") != 0)
-        errExit("putenv");
+	printf("Initial value of USER: %s\n", getenv("USER"));
 
-    /* exec printenv to display the USER and SHELL environment vars */
+	// set 'USER' to new value.
+	
+	if (putenv("USER=britta") != 0)
+		errExit("putenv");
 
-    execl("/usr/bin/printenv", "printenv", "USER", "SHELL", (char *) NULL);
-    errExit("execl");           /* If we get here, something went wrong */
+	/* exec printenv to display the USER and SHELL environment vars */
+
+	execl("/usr/bin/printenv", "printenv", "USER", "SHELL", (char *) NULL);
+
+	errExit("execl");           /* If we get here, something went wrong */
 }
