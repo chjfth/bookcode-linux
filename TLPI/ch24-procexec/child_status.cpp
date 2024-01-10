1598aec6b16f18f561ed4f47e73bc2f8b55c7d05
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-10 22:24:00 +0800

    CH26, child_status.cpp, make_zombie.cpp

--- TLPI/ch24-procexec/child_status.cpp
+++ TLPI/ch24-procexec/child_status.cpp
@@ -25,51 +25,63 @@
    normally or was killed by a signal.
 */
 #include <sys/wait.h>
+#include <assert.h>
 #include "print_wait_status.h"          /* Declares printWaitStatus() */
 #include "tlpi_hdr.h"
 
 int
 main(int argc, char *argv[])
 {
-    int status;
-    pid_t childPid;
+	int status;
+	pid_t childPid;
 
-    if (argc > 1 && strcmp(argv[1], "--help") == 0)
-        usageErr("%s [exit-status]\n", argv[0]);
+	if (argc > 1 && strcmp(argv[1], "--help") == 0)
+		usageErr("%s [exit-status]\n", argv[0]);
 
-    switch (fork()) {
-    case -1: errExit("fork");
+	switch (fork()) {
+	case -1: errExit("fork");
 
-    case 0:             /* Child: either exits immediately with given
-                           status or loops waiting for signals */
-        printf("Child started with PID = %ld\n", (long) getpid());
-        if (argc > 1)                   /* Status supplied on command line? */
-            exit(getInt(argv[1], 0, "exit-status"));
-        else                            /* Otherwise, wait for signals */
-            for (;;)
-                pause();
-        exit(EXIT_FAILURE);             /* Not reached, but good practice */
+	case 0:             /* Child: either exits immediately with given
+						   status or loops waiting for signals */
+		printf("Child started with PID = %ld\n", (long) getpid());
+		if (argc > 1)                   /* Status supplied on command line? */
+		{
+			exit(getInt(argv[1], 0, "exit-status"));
+		}
+		else                            /* Otherwise, wait for signals */
+		{
+			for (;;)
+				pause();
+		}
+		exit(EXIT_FAILURE);             /* Not reached, but good practice */
 
-    default:            /* Parent: repeatedly wait on child until it
-                           either exits or is terminated by a signal */
-        for (;;) {
-            childPid = waitpid(-1, &status, WUNTRACED
+	default:            /* Parent: repeatedly wait on child until it
+						   either exits or is terminated by a signal */
+		for (;;) 
+		{
+			childPid = waitpid(-1, &status, WUNTRACED
 #ifdef WCONTINUED       /* Not present on older versions of Linux */
-                                                | WCONTINUED
+												| WCONTINUED
 #endif
-                    );
-            if (childPid == -1)
-                errExit("waitpid");
+					);
+			if (childPid == -1)
+				errExit("waitpid");
 
-            /* Print status in hex, and as separate decimal bytes */
+			/* Print status in hex, and as separate decimal bytes */
 
-            printf("waitpid() returned: PID=%ld; status=0x%04x (%d,%d)\n",
-                    (long) childPid,
-                    (unsigned int) status, status >> 8, status & 0xff);
-            printWaitStatus(NULL, status);
+			printf("waitpid() returned: PID=%ld; status=0x%04x (%d,%d)\n",
+					(long) childPid,
+					(unsigned int) status, status >> 8, status & 0xff);
 
-            if (WIFEXITED(status) || WIFSIGNALED(status))
-                exit(EXIT_SUCCESS);
-        }
-    }
+			printWaitStatus(NULL, status);
+
+			if (WIFEXITED(status) || WIFSIGNALED(status))
+				exit(EXIT_SUCCESS);
+			else
+			{
+				// Chj test:
+				assert(WIFSTOPPED(status) || WIFCONTINUED(status));
+			}
+		}
+	}
 }
--- TLPI/ch24-procexec/make_zombie.cpp
+++ TLPI/ch24-procexec/make_zombie.cpp
@@ -26,34 +26,35 @@
 int
 main(int argc, char *argv[])
 {
-    char cmd[CMD_SIZE];
-    pid_t childPid;
+	char cmd[CMD_SIZE];
+	pid_t childPid;
 
-    setbuf(stdout, NULL);       /* Disable buffering of stdout */
+	setbuf(stdout, NULL);       /* Disable buffering of stdout */
 
-    printf("Parent PID=%ld\n", (long) getpid());
+	printf("Parent PID=%ld\n", (long) getpid());
 
-    switch (childPid = fork()) {
-    case -1:
-        errExit("fork");
+	switch (childPid = fork()) {
+	case -1:
+		errExit("fork");
 
-    case 0:     /* Child: immediately exits to become zombie */
-        printf("Child (PID=%ld) exiting\n", (long) getpid());
-        _exit(EXIT_SUCCESS);
+	case 0:     /* Child: immediately exits to become zombie */
+		printf("Child (PID=%ld) exiting\n", (long) getpid());
+		_exit(EXIT_SUCCESS);
 
-    default:    /* Parent */
-        sleep(3);               /* Give child a chance to start and exit */
-        snprintf(cmd, CMD_SIZE, "ps | grep %s", basename(argv[0]));
-        system(cmd);            /* View zombie child */
+	default:    /* Parent */
+		sleep(1);               /* Give child a chance to start and exit */
+		snprintf(cmd, CMD_SIZE, "ps | grep %s", basename(argv[0]));
+		system(cmd);            /* View zombie child */
 
-        /* Now send the "sure kill" signal to the zombie */
+		/* Now send the "sure kill" signal to the zombie */
 
-        if (kill(childPid, SIGKILL) == -1)
-            errMsg("kill");
-        sleep(3);               /* Give child a chance to react to signal */
-        printf("After sending SIGKILL to zombie (PID=%ld):\n", (long) childPid);
-        system(cmd);            /* View zombie child again */
+		if (kill(childPid, SIGKILL) == -1)
+			errMsg("kill");
 
-        exit(EXIT_SUCCESS);
-    }
+		sleep(2);               /* Give child a chance to react to signal */
+		printf("After sending SIGKILL to zombie (PID=%ld):\n", (long) childPid);
+		system(cmd);            /* View zombie child again */
+
+		exit(EXIT_SUCCESS);
+	}
 }
