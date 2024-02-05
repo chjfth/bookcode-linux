5aef54269c60bdca51ab228f8d160ed36a3b68f1
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-02-06 00:25:14 +0800

    Add CH33 strerror_tsd.cpp, thread-specific-data

--- TLPI/ch29-threads/strerror_test.cpp
+++ TLPI/ch29-threads/strerror_test.cpp
@@ -22,37 +22,37 @@
 static void *
 threadFunc(void *arg)
 {
-    char *str;
+	char *str;
 
-    printf("Other thread about to call strerror()\n");
-    str = strerror(EPERM);
-    printf("Other thread: str (%p) = %s\n", str, str);
+	printf("Other thread about to call strerror()\n");
+	str = strerror(EPERM);
+	printf("Other thread: str (%p) = %s\n", str, str);
 
-    return NULL;
+	return NULL;
 }
 
 int
 main(int argc, char *argv[])
 {
-    pthread_t t;
-    int s;
-    char *str;
+	pthread_t t;
+	int s;
+	char *str;
 
-    str = strerror(EINVAL);
-    printf("Main thread has called strerror()\n");
+	str = strerror(EINVAL);
+	printf("Main thread has called strerror()\n");
 
-    s = pthread_create(&t, NULL, threadFunc, NULL);
-    if (s != 0)
-        errExitEN(s, "pthread_create");
+	s = pthread_create(&t, NULL, threadFunc, NULL);
+	if (s != 0)
+		errExitEN(s, "pthread_create");
 
-    s = pthread_join(t, NULL);
-    if (s != 0)
-        errExitEN(s, "pthread_join");
+	s = pthread_join(t, NULL);
+	if (s != 0)
+		errExitEN(s, "pthread_join");
 
-    /* If strerror() is not thread-safe, then the output of this printf() be
-       the same as that produced by the analogous printf() in threadFunc() */
+	/* If strerror() is not thread-safe, then the output of this printf() be
+	   the same as that produced by the analogous printf() in threadFunc() */
 
-    printf("Main thread:  str (%p) = %s\n", str, str);
+	printf("Main thread:  str (%p) = %s\n", str, str);
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
