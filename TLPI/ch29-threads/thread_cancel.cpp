dfe63df4f8ba80d1dccae558b7d089dd26654a01
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-02-06 10:24:44 +0800

    Add CH32 thread cancellation code

--- TLPI/ch29-threads/thread_cancel.cpp
+++ TLPI/ch29-threads/thread_cancel.cpp
@@ -20,43 +20,43 @@
 static void *
 threadFunc(void *arg)
 {
-    int j;
+	int j;
 
-    printf("New thread started\n");     /* May be a cancellation point */
-    for (j = 1; ; j++) {
-        printf("Loop %d\n", j);         /* May be a cancellation point */
-        sleep(1);                       /* A cancellation point */
-    }
+	printf("New thread started\n");     /* May be a cancellation point */
+	for (j = 1; ; j++) {
+		printf("Loop %d\n", j);         /* May be a cancellation point */
+		sleep(1);                       /* A cancellation point */
+	}
 
-    /* NOTREACHED */
-    return NULL;
+	/* NOTREACHED */
+	return NULL;
 }
 
 int
 main(int argc, char *argv[])
 {
-    pthread_t thr;
-    int s;
-    void *res;
+	pthread_t thr;
+	int s;
+	void *res;
 
-    s = pthread_create(&thr, NULL, threadFunc, NULL);
-    if (s != 0)
-        errExitEN(s, "pthread_create");
+	s = pthread_create(&thr, NULL, threadFunc, NULL);
+	if (s != 0)
+		errExitEN(s, "pthread_create");
 
-    sleep(3);                           /* Allow new thread to run a while */
+	sleep(3);                           /* Allow new thread to run a while */
 
-    s = pthread_cancel(thr);
-    if (s != 0)
-        errExitEN(s, "pthread_cancel");
+	s = pthread_cancel(thr);
+	if (s != 0)
+		errExitEN(s, "pthread_cancel");
 
-    s = pthread_join(thr, &res);
-    if (s != 0)
-        errExitEN(s, "pthread_join");
+	s = pthread_join(thr, &res);
+	if (s != 0)
+		errExitEN(s, "pthread_join");
 
-    if (res == PTHREAD_CANCELED)
-        printf("Thread was canceled\n");
-    else
-        printf("Thread was not canceled (should not happen!)\n");
+	if (res == PTHREAD_CANCELED)
+		printf("Thread was canceled\n");
+	else
+		printf("Thread was not canceled (should not happen!)\n");
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
