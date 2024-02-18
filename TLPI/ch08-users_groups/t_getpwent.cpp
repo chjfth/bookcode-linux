ac812a76d54866cfa81243cecd56d99c8bffa5d5
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-02-18 17:16:29 +0800

    CH08, t_getpwend()

--- TLPI/ch08-users_groups/t_getpwent.cpp
+++ TLPI/ch08-users_groups/t_getpwent.cpp
@@ -21,10 +21,10 @@
 int
 main(int argc, char *argv[])
 {
-    struct passwd *pwd;
+	struct passwd *pwd;
 
-    while ((pwd = getpwent()) != NULL)
-        printf("%-8s %5ld\n", pwd->pw_name, (long) pwd->pw_uid);
-    endpwent();
-    exit(EXIT_SUCCESS);
+	while ((pwd = getpwent()) != NULL)
+		printf("%-8s %5ld\n", pwd->pw_name, (long) pwd->pw_uid);
+	endpwent();
+	exit(EXIT_SUCCESS);
 }
