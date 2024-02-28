6ce6b009227484cf8eceaa735c556d645f60800f
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-02-28 23:36:26 +0800

    CH39 show_secbits.cpp add test code prctl(PR_SET_SECUREBITS,...)

--- TLPI/ch39-cap/show_secbits.cpp
+++ TLPI/ch39-cap/show_secbits.cpp
@@ -12,21 +12,52 @@
 
 /* show_secbits.c
 */
+#include <linux/securebits.h>
 #include <sys/prctl.h>
 #include "cap_functions.h"
 #include "tlpi_hdr.h"
 
+void p812_make_me_purely_capabase() // Chj adds this
+{
+	int err = prctl(PR_SET_SECUREBITS,
+		// SECBIT_KEEP_CAPS off 
+		SECBIT_NO_SETUID_FIXUP | SECBIT_NO_SETUID_FIXUP_LOCKED |
+		SECBIT_NOROOT | SECBIT_NOROOT_LOCKED);
+	if(err)
+	{
+		printf("prctl(PR_SET_SECUREBITS,...) error. errno=%d (%s)\n",
+			errno, strerror(errno));
+	}
+	else
+	{
+		printf("prctl(PR_SET_SECUREBITS,...) success.\n");
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
+	if (argc > 1)
+		p812_make_me_purely_capabase();
+	
+	printf("Calling prctl(PR_GET_SECUREBITS)...\n");
 	int secbits = prctl(PR_GET_SECUREBITS, 0, 0, 0, 0);
 	if (secbits == -1)
 		errExit("prctl");
 
 	printf("Self-process secbits = 0x%x => ", secbits);
 
-	printSecbits(secbits, argc == 1, stdout);
+	printSecbits(secbits, false, stdout);
 	printf("\n");
 
+	printSecbits(secbits, true, stdout);
+	printf("\n");
+
+	if(argc>1)
+	{
+		printf("\nProgram done. Enter to quit.\n");
+		getchar();
+	}
+	
 	exit(EXIT_SUCCESS);
 }
