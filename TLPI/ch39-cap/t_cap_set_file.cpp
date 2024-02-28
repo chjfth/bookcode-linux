45c9930a0ebee994d8aed6659b19253b4322f1d2
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-02-28 13:49:10 +0800

    CH39 t_cap_set_file.cpp

--- TLPI/ch39-cap/t_cap_set_file.cpp
+++ TLPI/ch39-cap/t_cap_set_file.cpp
@@ -20,29 +20,26 @@
 int
 main(int argc, char *argv[])
 {
-    cap_t capSets;
-    char *textCaps;
+	if (argc != 3) {
+		fprintf(stderr, "%s <textual-cap-set> <pathname>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    if (argc != 3) {
-        fprintf(stderr, "%s <textual-cap-set> <pathname>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	cap_t capSets = cap_from_text(argv[1]);
+	if (capSets == NULL)
+		errExit("cap_from_text");
 
-    capSets = cap_from_text(argv[1]);
-    if (capSets == NULL)
-        errExit("cap_from_text");
+	char *textCaps = cap_to_text(capSets, NULL);
+	if (textCaps == NULL)
+		errExit("cap_to_text");
 
-    textCaps = cap_to_text(capSets, NULL);
-    if (textCaps == NULL)
-        errExit("cap_to_text");
+	printf("caps_to_text() returned \"%s\"\n\n", textCaps);
 
-    printf("caps_to_text() returned \"%s\"\n\n", textCaps);
+	if (cap_set_file(argv[2], capSets) == -1)
+		errExit("cap_set_file");
 
-    if (cap_set_file(argv[2], capSets) == -1)
-        errExit("cap_set_file");
+	if (cap_free(textCaps) != 0 || cap_free(capSets) != 0)
+		errExit("cap_free");
 
-    if (cap_free(textCaps) != 0 || cap_free(capSets) != 0)
-        errExit("cap_free");
-
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
